4ecf66d712d87e3e37e817f2df14c0332e9d89a6
Serhii Meshcheriakov <sergeimeshcheryakov14@gmail.com>
2026-04-15 16:42:16 +0300

    Init commit

---
+++ src/main/java/serhii/meshcheriakov/springlearn1/Springlearn1Application.java
@@ -0,0 +1,13 @@
+package serhii.meshcheriakov.springlearn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlearn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlearn1Application.class, args);
+	}
+
+}
---
+++ src/test/java/serhii/meshcheriakov/springlearn1/Springlearn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package serhii.meshcheriakov.springlearn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlearn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
