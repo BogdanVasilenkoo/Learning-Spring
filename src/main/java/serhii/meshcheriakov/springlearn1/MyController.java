b45f2059e201d58d245a7edf5fcdb2a2057199ef
Serhii Meshcheriakov <sergeimeshcheryakov14@gmail.com>
2026-04-17 12:53:10 +0300

    added delete and put Mapping

--- src/main/java/serhii/meshcheriakov/springlearn1/MyController.java
+++ src/main/java/serhii/meshcheriakov/springlearn1/MyController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import serhii.meshcheriakov.springlearn1.model.Order;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/order")
 public class MyController {
     private static final Logger logger = LoggerFactory.getLogger(MyController.class);
 
@@ -24,8 +26,8 @@ public class MyController {
         return myService.getOrderById(id);
     }
 
-    @GetMapping()
-    public ResponseEntity<List<Order>> getAllOrders() {
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders(){
         logger.info("getAllOrders");
         return ResponseEntity.ok(myService.getAllOrders());
     }
@@ -34,7 +36,28 @@ public class MyController {
     public ResponseEntity<Order> addOrder(@RequestBody Order newOrder) {
         logger.info("addOrder");
         return ResponseEntity.status(201)
-                .header("test-header", "123")
                 .body(myService.addOrder(newOrder));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        logger.info("deleteOrder");
+        try {
+            myService.deleteOrder(id);
+        } catch (NoSuchElementException e) {
+            logger.info(e.getMessage());
+            return ResponseEntity.status(404).build();
+        } catch (IllegalArgumentException e) {
+            logger.info(e.getMessage());
+            return ResponseEntity.status(409).build();
+        }
+        return ResponseEntity.status(200)
+                .build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order updatedOrder) {
+        logger.info("updateOrder");
+        return ResponseEntity.ok(myService.updateOrder(id, updatedOrder));
     }
 }
--- src/main/java/serhii/meshcheriakov/springlearn1/MyService.java
+++ src/main/java/serhii/meshcheriakov/springlearn1/MyService.java
@@ -1,5 +1,6 @@
 package serhii.meshcheriakov.springlearn1;
 
+import org.jspecify.annotations.Nullable;
 import serhii.meshcheriakov.springlearn1.model.Order;
 import org.springframework.stereotype.Service;
 import serhii.meshcheriakov.springlearn1.model.OrderStatus;
@@ -48,5 +49,35 @@ public class MyService {
         );
         orders.put(order.id(), order);
         return order;
+    }
+
+    public void deleteOrder(Long id) {
+        if (!orders.containsKey(id)) {
+            throw new NoSuchElementException("Order not found! Id = " + id);
+        }
+        var order = orders.get(id);
+        if (order.status() == OrderStatus.IN_TRANSIT) {
+            throw new IllegalArgumentException("Order status should not be IN_TRANSIT!");
+        }
+        orders.remove(id);
+    }
+
+    public Order updateOrder(Long id, Order updatedOrder) {
+        if (!orders.containsKey(id)) {
+            throw new NoSuchElementException("Order not found! Id = " + id);
+        }
+        var order = orders.get(id);
+        if (order.status() != OrderStatus.IN_PROCESS) {
+            throw new IllegalArgumentException("Order status should be IN_PROCESS!");
+        }
+        var newOrder = new Order(
+                order.id(),
+                updatedOrder.user_id(),
+                updatedOrder.product_id(),
+                updatedOrder.amount_to_be_paid(),
+                OrderStatus.IN_PROCESS
+        );
+        orders.put(id, newOrder);
+        return newOrder;
     }
 }
